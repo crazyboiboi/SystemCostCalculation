f99afe340abe89388d204d87425c326c052e8624
crazyboiboi <32148889+crazyboiboi@users.noreply.github.com>
2019-07-31 13:06:59 +0800

    New button functionality added, can change between views and viewmodels now.

--- SystemCostCalculation/ViewModels/MainWindowViewModel.cs
+++ SystemCostCalculation/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,84 @@ using System.Threading.Tasks;
 
 namespace SystemCostCalculation.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ViewModelBase
     {
+        private ManageItemViewModel manageItemViewModel;
+        private ManageSupplierViewModel manageSupplierViewModel;
+
+
+        public MainWindowViewModel()
+        {
+            manageItemViewModel = new ManageItemViewModel();
+            manageSupplierViewModel = new ManageSupplierViewModel();
+        }
+
+
+
+        private object _currentViewModel;
+        public object currentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                if (_currentViewModel == value)
+                    return;
+                _currentViewModel = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private RelayCommand itemTabCommand; 
+        public RelayCommand ItemTabCommand
+        {
+            get
+            {
+                if(itemTabCommand == null)
+                {
+                    itemTabCommand = new RelayCommand(() =>
+                   {
+                       setView("item");
+                   });
+                }
+                return itemTabCommand;
+            }
+        }
+
+
+        private RelayCommand supplierTabCommand; 
+        public RelayCommand SupplierTabCommand
+        {
+            get
+            {
+                if(supplierTabCommand == null)
+                {
+                    supplierTabCommand = new RelayCommand(() =>
+                   {
+                       setView("supplier");
+                   });
+                }
+                return supplierTabCommand;
+            }
+        }
+
+
+
+
+
+        private void setView(string name)
+        {
+            if (name.Equals("item"))
+            {
+                currentViewModel = manageItemViewModel;
+            } else if (name.Equals("supplier"))
+            {
+                currentViewModel = manageSupplierViewModel;
+            }
+        }
+
+
     }
 }
