c1b29756685c0ee259cd36651e9e4997d6605674
crazyboiboi <32148889+crazyboiboi@users.noreply.github.com>
2019-08-01 22:44:00 +0800

    Set up a small section ManageSupplierView UserControl to use its ManageSupplierViewModel class for future reference and build up. ManageItemView design is almost completed, waiting for further comments and improvement remarks.

--- SystemCostCalculation/ViewModels/ManageSupplierViewModel.cs
+++ SystemCostCalculation/ViewModels/ManageSupplierViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace SystemCostCalculation.ViewModels
 {
-    class ManageSupplierViewModel
+    class ManageSupplierViewModel : ViewModelBase
     {
+
+        public ManageSupplierViewModel()
+        {
+        }
+
+
+        //This is for debugging purpose
+        private RelayCommand debugCommand;
+        public RelayCommand DebugCommand
+        {
+            get
+            {
+                if(debugCommand == null)
+                {
+                    debugCommand = new RelayCommand(() =>
+                   {
+                       Console.WriteLine("HELLO WORLD");
+                   });
+                }
+                return debugCommand;
+            }
+        }
+
+
     }
 }
